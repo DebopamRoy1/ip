8f0e05683397b3be1483de0ce2cbf42ba44cc2de
Debopam Roy <rhevehjsns@gmail.com>
2026-01-22 15:05:54 +0800

    Level-1: Improved Lebron to echo user inputs and exit when instructed

--- src/main/java/Lebron.java
+++ src/main/java/Lebron.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Lebron {
     public static void main(String[] args) {
-        String greeting = "Yo yo yo what's up its your boy LeBron James haha.\n" +
-                "What can I do to help you?\n";
-        String seperate = "___________________________\n";
-        String exit = "That's all from your boy from Akron, Ohio.\n" +
-                "#striveForGreatness #appreciateGreatness\n";
-        System.out.println(greeting + seperate + exit + seperate);
+        String line = "_____________________________________________________________________________\n";
+
+        //Greeting
+        System.out.println(line);
+        System.out.println("Yo yo yo, what's up it's your boy LeBron James haha. ");
+        System.out.println("Just a kid from Akron, Ohio here to help you. What's on your mind?");
+        System.out.println(line);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            // Check if user wants to exit
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(line);
+                System.out.println("That's all from your boy from Akron, Ohio");
+                System.out.println("#striveforgreatness #appreciategreatness");
+                System.out.println(line);
+                break;
+            }
+
+            // Echo the user input
+            System.out.println(line);
+            System.out.println("That's what's up King! You said: " + input);
+            System.out.println(line);
+        }
+
+        scanner.close();
     }
 }
