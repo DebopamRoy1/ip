ec0378563e514ff692a9cf2e5532e3687f2b2395
Debopam Roy <rhevehjsns@gmail.com>
2026-01-22 14:52:50 +0800

    Renamed Duke to Lebron and added greeting/exit

---
+++ src/main/java/Lebron.java
@@ -0,0 +1,10 @@
+public class Lebron {
+    public static void main(String[] args) {
+        String greeting = "Yo yo yo what's up its your boy LeBron James haha.\n" +
+                "What can I do to help you?\n";
+        String seperate = "___________________________\n";
+        String exit = "That's all from your boy from Akron, Ohio.\n" +
+                "#striveForGreatness #appreciateGreatness\n";
+        System.out.println(greeting + seperate + exit + seperate);
+    }
+}
