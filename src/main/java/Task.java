6b608b86cea5804a94e9d833be3f70db7a55ce89
Debopam Roy <rhevehjsns@gmail.com>
2026-01-27 19:54:56 +0800

    Level-7: Implement file persistence and more comments on code

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,4 @@
-public class Task {
+public abstract class Task {
     protected String description;
     protected boolean isDone;
 
@@ -7,8 +7,12 @@ public class Task {
         this.isDone = false;
     }
 
+    /**
+     * Returns "X", representing the completion status of the task.
+     *
+     * @return "X" if the task is done, and a blank space otherwise.
+     */
     public String getStatusIcon() {
-        // Mark done task with a 'X'
         if (isDone) {
             return "X";
         } else {
@@ -24,9 +28,24 @@ public class Task {
         this.isDone = false;
     }
 
+    /**
+     * Returns binary representation of the task for file storage.
+     *
+     * @return "1" if done, "0" otherwise.
+     */
+    public String getStatusBinary() {
+        return (isDone ? "1" : "0");
+    }
+
     @Override
     public String toString() {
         return "[" + getStatusIcon() + "]" + description;
     }
 
+    /**
+     * Returns the string representation of the task formatted for a text file.
+     *
+     * @return Formatted string for saving to disk.
+     */
+    public abstract String toFileFormat();
 }
